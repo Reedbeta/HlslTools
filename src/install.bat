0f01204f817aec2cc1317c617b88991dc4b34d49
Nathan Reed <nathaniel.reed@gmail.com>
2019-07-13 14:00:32 -0700

    Add batch script for un/reinstalling the extension
    Iteration is better done using the "experimental instance" feature of VS, but keeping the batch file for future reference

---
+++ src/install.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe" /u:7def6c01-a05e-42e6-953d-3fdea1891737
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe" "D:\Source\HlslTools\src\ShaderTools.Editor.VisualStudio\bin\Debug\ShaderTools.Editor.VisualStudio.vsix"
